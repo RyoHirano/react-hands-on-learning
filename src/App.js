206078bf825a8512c07a3d3faaba4c1ab28462c1
ryo.hirano <study.hellbird@gmail.com>
2021-08-22 10:07:08 +0900

    add color form.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import colorData from "./color-data.json";
 import ColorList from "./ColorList";
+import AddColorForm from "./AddColorForm";
 
 function App() {
   const [colors, setColors] = useState(colorData);
+
+  const rateColor = (id, rating) => {
+    const newColors = colors.map((color) =>
+      color.id === id ? { ...color, rating } : color
+    );
+    setColors(newColors);
+  };
+
+  const removeColor = (id) => {
+    const newColors = colors.filter((color) => color.id !== id);
+    setColors(newColors);
+  };
+
   return (
-    <div className="App">
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => alert(`TODO: Create ${title} - ${color}`)}
+      />
       <ColorList
         colors={colors}
-        onRateColor={(id, rating) => {
-          const newColors = colors.map((color) =>
-            color.id === id ? { ...color, rating } : color
-          );
-          setColors(newColors);
-        }}
-        onRemoveColor={(id) => {
-          const newColors = colors.filter((color) => color.id !== id);
-          setColors(newColors);
-        }}
+        onRateColor={rateColor}
+        onRemoveColor={removeColor}
       />
-    </div>
+    </>
   );
 }
 
